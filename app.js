04ee7fbebe77b5dab854a27ceca09b8346bf2a55
Seraphim Lee <sjl073@uw.edu>
2020-06-10 22:26:36 -0700

    div
    FOUND IT

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Link to the MTV article//
-
 <script>
     function visitPage(){
         document.location='http://www.mtv.com/news/3163884/ateez-became-unstoppable-interview/'
@@ -8,10 +7,34 @@
 <button onclick="visitPage();">Read More</button>
 
 //Link to Ticketmaster//
-
 <script>
     function visitPage(){
         document.location='https://www.ticketmaster.com/ateez-tickets/artist/2595862'
     };
 </script>
 <button onclick="visitPage();">help?</button>
+
+//Link to merch store//
+<script>
+    function visitPage(){
+        document.location='https://global.yes24.com/search?q=ATEEZ'
+    }
+</script>
+<button onclick="visitPage();">Merchandise</button>
+
+//Link to Answer MV//
+<script>
+    function visitPage(){
+        document.location='https://www.youtube.com/watch?v=yW7wZX3DUaY'
+    }    
+</script>
+<button onclick="visitPage();">Extras</button>
+
+
+//Link to KQ Entertainment Youtube Channel//
+<script>
+    function visitPage(){
+        document.location='https://www.youtube.com/channel/UCQdq-lqPEq_yZ_wP_kuVB9Q'
+    }    
+</script>
+<button onclick="visitPage();">Media</button>
